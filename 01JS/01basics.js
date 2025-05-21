84c5dfdc720469aa6d3e54dd27e39dfe97b42b11
Sakshi Sharma <109201191+sakshi-142000@users.noreply.github.com>
2025-05-21 12:51:10 +0000

    Basic Datatypes Revision

--- 01JS/01basics.js
+++ 01JS/01basics.js
@@ -1,2 +1,51 @@
-"use strict";
-console.log("sakshi");
+"use strict"; //using the new js
+// console.log("sakshi");
+
+// discussing js datatype today
+
+/*we can not use the alert statement in nodejs runtime js, 
+
+this is a browser specific function, browser used to provide it using the "window" object
+In node.js, we do not have DOM, or window object so alert does not exist. 
+*/
+
+//alert("hello"); we have a different syntax available for alert in node
+
+// semicolon is not required in js, howevever readability is required
+
+//original docs: ECMA Script: https://tc39.es/ecma262/
+// other one is MDN (user-friendly)
+
+let name="sakshi"; //string, can be single or double quote
+let age=18; //2^53 something range of number
+
+
+/*
+
+Datatypes:
+Primitive:
+number, 
+bigint
+string
+boolean
+null: Stand alone value (later discussion) (is empty no undefined)
+undefined: when you declare a value, js gives it undefined default value, i,e value is not assigned
+symbol: for finding uniqueness in the component
+
+Non primitive
+Object (imp)
+*/
+
+//the typeof operator, you can declare it using three ways
+console.log(typeof(name));
+console.log(typeof name);
+console.log(typeof "sakshi");
+
+console.log(typeof null); //gives an object, null is an object
+console.log(typeof undefined); //gives undefined
+
+
+
+
+
+
