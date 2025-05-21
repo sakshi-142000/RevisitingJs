0a00354107e2d92b22fbdde995c661f0fd9c585c
Sakshi Sharma <109201191+sakshi-142000@users.noreply.github.com>
2025-05-21 14:39:49 +0000

    Operations Corner Cases

---
+++ 01JS/Operations.js
@@ -0,0 +1,17 @@
+let value=3
+let negValue=-value;
+console.log(value);
+console.log(negValue);
+console.log(2**3) // this is 2^3
+/*String concatenation Rules in JS
+
+string + number -> result will be string
+Number + String -> numeric calculation will first happen with the numbers and then concatenation
+
+*/
+
+console.log("1"+"2"); //"12"
+console.log("1"+2); // "12"
+console.log(1+2+ "3"); //"33" 
+console.log(+"") //0
+console.log(+true) //1
